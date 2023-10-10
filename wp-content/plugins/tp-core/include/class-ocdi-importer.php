11a8e31afd0d8b6c7d3c4015fbf434afef77f3e1
samrat87573015 <www.samrat1998@gmail.com>
2023-10-10 17:19:11 +0600

    fixed

--- wp-content/plugins/tp-core/include/class-ocdi-importer.php
+++ wp-content/plugins/tp-core/include/class-ocdi-importer.php
@@ -8,32 +8,15 @@ function tp_ocdi_import_files() {
     
     return array(
       array(
-        'import_file_name'           => 'Home 1',
+        'import_file_name'           => 'Main Home',
         // 'categories'                 => array( 'Category 1', 'Category 2' ),
         'local_import_file'             => trailingslashit( get_template_directory() ) .'sample-data/contents-demo.xml',
         'local_import_widget_file' => trailingslashit( get_template_directory() ) . 'sample-data/widget-settings.json',
         'local_import_customizer_file' => trailingslashit( get_template_directory() ) . 'sample-data/customizer-data.dat',
         'import_preview_image_url' => plugins_url( 'assets/img/demo/home-1.jpg', dirname(__FILE__) ),
-        'preview_url'                => 'https://weblearnbd.net/wp/ninico',
+        'preview_url'                => 'https://smartthemebd.com/wp/shofa/',
       ),
-      array(
-        'import_file_name'           => 'Home 2',
-        // 'categories'                 => array( 'New category 1', 'Old category 2' ),
-        'local_import_file'             => trailingslashit( get_template_directory() ) .'sample-data/contents-demo.xml',
-        'local_import_widget_file' => trailingslashit( get_template_directory() ) . 'sample-data/widget-settings.json',
-        'local_import_customizer_file' => trailingslashit( get_template_directory() ) . 'sample-data/customizer-data.dat',
-        'import_preview_image_url' => plugins_url( 'assets/img/demo/home-2.jpg', dirname(__FILE__) ),
-        'preview_url'                => 'https://weblearnbd.net/wp/ninico/home-2/',
-      ),
-      array(
-        'import_file_name'           => 'Home 3',
-        // 'categories'                 => array( 'New category', 'Old category' ),
-        'local_import_file'             => trailingslashit( get_template_directory() ) .'sample-data/contents-demo.xml',
-        'local_import_widget_file' => trailingslashit( get_template_directory() ) . 'sample-data/widget-settings.json',
-        'local_import_customizer_file' => trailingslashit( get_template_directory() ) . 'sample-data/customizer-data.dat',
-        'import_preview_image_url' => plugins_url( 'assets/img/demo/home-3.jpg', dirname(__FILE__) ),
-        'preview_url'                => 'https://weblearnbd.net/wp/ninico/home-3/',
-      ),
+
     );
 }
 add_filter( 'ocdi/import_files', 'tp_ocdi_import_files' );
@@ -72,10 +55,10 @@ function tp_ocdi_after_import_setup( $demo ) {
 
     // woocommerce default settings reset
     if ( class_exists( 'woocommerce' ) ) {
-        update_option( 'woocommerce_shop_page_id', '13' );
-        update_option( 'woocommerce_cart_page_id', '14' );
+        update_option( 'woocommerce_shop_page_id', '8' );
+        update_option( 'woocommerce_cart_page_id', '9' );
         // update_option( 'woocommerce_checkout_page_id', '15' );
-        update_option( 'woocommerce_myaccount_page_id', '16' );
+        update_option( 'woocommerce_myaccount_page_id', '11' );
     }
  
 }
